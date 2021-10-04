9ddd7ce2558e052fb0abe76fed7e880343fcfd80
lzhnb <liang13268932888@outlook.com>
2021-10-04 16:52:23 +0800

    use the which-key to realize the mapping of coc-git

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,17 +5,25 @@ local mappings = {
     f = {
         name = "file-telescope", -- optional group name
         -- create bindings
-        f = { "<cmd>Telescope find_files<cr>", "Find File" },       -- find file using telescope(ctrl-p)
-        b = { "<cmd>Telescope find_buffers<cr>", "Find Buffer" },   -- find buffer using telescope
-        g = { "<cmd>Telescope live_grep<cr>", "live grep" },        -- grep using telescope
-        h = { "<cmd>Telescope help_tags<cr>", "telescope healp" },  -- telescope help
-        n = { "<cmd>enew<cr>", "New File" },                        -- create a new empty buffer
-        ["1"] = "which_key_ignore",                                 -- special label to hide it in the popup
+        f = {":Telescope find_files<CCR>", "Find File"},       -- find file using telescope(ctrl-p)
+        b = {":Telescope find_buffers<CR>", "Find Buffer"},    -- find buffer using telescope
+        g = {":Telescope live_grep<CR>", "live grep"},         -- grep using telescope
+        h = {":Telescope help_tags<CR>", "telescope healp"},   -- telescope help
+        n = {":enew<CR>", "New File"},                         -- create a new empty buffer
+        ["1"] = "which_key_ignore",                            -- special label to hide it in the popup
+    },
+    g = {
+        name = "git",
+        h = {":CocCommand git.chunkInfo<CR>", "Info Hunk"},    -- show the git hunk information
+        s = {":CocCommand git.chunkStage<CR>", "Stage Hunk"},  -- save the hunk change
+        u = {":CocCommand git.chunkUndo<CR>", "Undo Hunk"},    -- undo the hunk
+        c = {":CocCommand git.showCommit<CR>", "Show Commit"},  -- show commit information
     }
 }
 
-local opts = { prefix = "<leader>" }
+local opts = {prefix = "<leader>"}
 
 wk.register(mappings, opts)
+
 
 
